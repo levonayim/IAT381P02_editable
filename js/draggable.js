c0d41829319a90dac3989650d13884972f5ee520
levonayim <levona10@hotmail.com>
2015-02-28 22:51:38 -0800

    able to add new "note" draggable BUT can only drag entire thing, can't drag individual for now

--- js/draggable.js
+++ js/draggable.js
@@ -3,9 +3,9 @@ See http://www.greensock.com/draggable/ for details.
 This demo uses ThrowPropsPlugin which is a membership benefit of Club GreenSock, http://www.greensock.com/club/
 */
 
-Draggable.create(".polaroid", {type:"x,y", edgeResistance:0.65, bounds:"#container", throwProps:true});
+Draggable.create("#add_new", {type:"x,y", edgeResistance:0.65, bounds:".zoomwrapper", throwProps:true});
 
-// Draggable.create(".zoomProps", {type: "rotation", throwProps: true});
+//Draggable.create("#add_new", {type: "rotation", throwProps: true});
 
 //positional velocity
 Draggable.create("#movableID", {
@@ -27,7 +27,7 @@ Draggable.create("#scrollableID", {
 
 var $snap = $("#snap"),
   $liveSnap = $("#liveSnap"),
-	$container = $("#container"),
+	$zoomwrapper = $(".zoomwrapper"),
 	gridWidth = 196,
 	gridHeight = 100,
 	gridRows = 6,
@@ -38,19 +38,19 @@ var $snap = $("#snap"),
 for (i = 0; i < gridRows * gridColumns; i++) {
 	y = Math.floor(i / gridColumns) * gridHeight;
 	x = (i * gridWidth) % (gridColumns * gridWidth);
-	$("<div/>").css({position:"absolute", border:"1px solid #454545", width:gridWidth-1, height:gridHeight-1, top:y, left:x}).prependTo($container);
+	$("<div/>").css({position:"absolute", border:"1px solid #454545", width:gridWidth-1, height:gridHeight-1, top:y, left:x}).prependTo($zoomwrapper);
 }
 
-//set the container's size to match the grid, and ensure that the box widths/heights reflect the variables above
-TweenLite.set($container, {height: gridRows * gridHeight + 1, width: gridColumns * gridWidth + 1});
-TweenLite.set(".polaroid", {width:gridWidth, height:gridHeight, lineHeight:gridHeight + "px"});
+//set the zoomwrapper's size to match the grid, and ensure that the box widths/heights reflect the variables above
+TweenLite.set($zoomwrapper, {height: gridRows * gridHeight + 1, width: gridColumns * gridWidth + 1});
+TweenLite.set("#add_new", {width:gridWidth, height:gridHeight, lineHeight:gridHeight + "px"});
 
 //the update() function is what creates the Draggable according to the options selected (snapping).
 function update() {
   var snap = $snap.prop("checked"),
       liveSnap = $liveSnap.prop("checked");
-	Draggable.create(".polaroid", {
-		bounds:$container,
+	Draggable.create("#add_new", {
+		bounds:$zoomwrapper,
 		edgeResistance:0.65,
 		type:"x,y",
 		throwProps:true,
@@ -72,7 +72,7 @@ $liveSnap.on("change", applySnap);
 
 function applySnap() {
 	if ($snap.prop("checked") || $liveSnap.prop("checked")) {
-		$(".polaroid").each(function(index, element) {
+		$("#add_new").each(function(index, element) {
 			TweenLite.to(element, 0.5, {
 				x:Math.round(element._gsTransform.x / gridWidth) * gridWidth,
 				y:Math.round(element._gsTransform.y / gridHeight) * gridHeight,
